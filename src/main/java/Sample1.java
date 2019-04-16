5094ef59412add2c6838753b6dee3116cc6384cf
Saeed Shariati <sd.shariati@gmail.com>
2019-04-16 11:24:13 +0430

    Init commit

---
+++ src/main/java/Sample1.java
@@ -0,0 +1,32 @@
+import br.com.tempus.iot.sdk.*;
+import br.com.tempus.iot.sdk.errors.*;
+
+import java.util.Date;
+
+public class Sample1 {
+    public static void main(String[] args) {
+        Connector connector = new Connector();
+        try {
+            connector.connect("API_KEY");
+            for (int i = 0; i < 10; i++) {
+                long timestamp = new Date().getTime();
+                Record r = new Record(timestamp, "S1", Record.SensorTypes.RFID);
+                r.Data().setTag("e20068060000000000000021");
+                connector.send(r);
+            }
+        }
+        catch (InvalidKeyException e) {
+            throw e;
+        }
+        catch (InitializationException e) {
+            throw e;
+        }
+        catch (RuntimeException e) {
+            throw e;
+        }
+        finally {
+            connector.flush();
+            connector.close();
+        }
+    }
+}
